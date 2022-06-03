332955d5ee5790d24f2b19038ede0b85d3a7cf62
Max Church <maxchurch01@gmail.com>
2022-06-03 20:04:48 +0100

    fiddling with urls

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,8 +29,8 @@ const Home = () => {
             <h1 className='text-4xl  text-center'>Max Church</h1>
             <h2 className='md:text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
             <div className='aspect-video bg-black m-10'>
-            <iframe className='w-full h-full' src="https://www.youtube.com/embed/kLBtY_rneF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
+            <iframe className='w-full h-full bg-black' src="https://www.youtube.com/embed/kLBtY_rneF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
           </div>
 
           <div className='px-6 text-white space-y-5 flex flex-col'>
