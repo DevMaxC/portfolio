816b6d39343c34d7093603b5d0cb251ce124c907
Max Church <maxchurch01@gmail.com>
2022-06-03 19:41:04 +0100

    added locale

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,8 +11,9 @@ const Home = () => {
         <title>Max Church</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Max Church - Full Stack Develloper" />
       </Head>
-      
+
       <div className='min-w-screen min-h-screen bg-slate-600'>
         <nav className='z-20 py-5 flex justify-evenly sticky top-0 bg-slate-700 opacity-100 text-white md:text-2xl font-thin'>
           <Link href="/"><a className='hover:text-slate-400'>Home</a></Link>
