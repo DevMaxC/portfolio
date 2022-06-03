63c1eccb4a187789902ccc62d820b9639a9eff27
Max Church <maxchurch01@gmail.com>
2022-06-03 20:21:05 +0100

    optimised

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -6,7 +6,7 @@ function Skill(props) {
         <div className='flex space-x-5 md:space-x-10 items-center shadow-2xl bg-slate-500 rounded-xl'>
             <div className='h-24 w-24 rounded-l-xl bg-white shrink-0 p-2'>
                 
-                <Image alt={props.title + " logo"} height={200} width={200} src={props.src}></Image>
+                <Image alt={`${props.title} logo`} height={"200px"} width={"200px"} src={props.src}></Image>
             </div>
             <div className='flex flex-col md:flex-row md:gap-10'>
                 <h2 className='sm:text-2xl'>{props.title}</h2>
