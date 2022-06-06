f872b6f331041225eb0b27576eced688f37a9fee
Max Church <maxchurch01@gmail.com>
2022-06-06 20:57:47 +0100

    redesign of homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import Skill from '/components/Skill'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Skill from "/components/Skill";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 const Home = () => {
-  return(
+  const [text, textSetter] = useState("");
+  useEffect(() => {}, [text]);
+  return (
     <div>
       <Head>
         <title>Max Church</title>
@@ -14,46 +17,48 @@ const Home = () => {
         <meta name="description" content="Max Church - Full Stack Develloper" />
       </Head>
 
-      <div className='min-w-screen min-h-screen bg-slate-600'>
-        <nav className='z-20 py-5 flex justify-evenly sticky top-0 bg-slate-700 opacity-100 text-white md:text-2xl font-thin'>
-          <Link href="/"><a className='hover:text-slate-400'>Home</a></Link>
-          <Link href="/"><a className='hover:text-slate-400'>About</a></Link>
-          <Link href="/"><a className='hover:text-slate-400'>Elements</a></Link>
-          <Link href="/"><a className='hover:text-slate-400'>Other</a></Link>
-
+      <div className="min-w-screen min-h-screen">
+        <nav className="sticky top-0 z-20 flex justify-evenly bg-red-100  py-5 font-light text-black opacity-100 md:text-2xl">
+          <Link href="/">
+            <a className="transition-colors hover:text-slate-600">Home</a>
+          </Link>
+          <Link href="/">
+            <a className="transition-colors hover:text-slate-600">About</a>
+          </Link>
+          <Link href="/">
+            <a className="transition-colors hover:text-slate-600">Elements</a>
+          </Link>
+          <Link href="/">
+            <a className="transition-colors hover:text-slate-600">Other</a>
+          </Link>
         </nav>
 
-        <main className='grid grid-cols-1 lg:grid-cols-2 gap-6 py-6'>
-
-          <div className='px-6 text-white space-y-6'>
-            <h1 className='text-4xl  text-center'>Max Church</h1>
-            <h2 className='md:text-2xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-            <div className='aspect-video bg-black m-5'>
-              <iframe className='w-full h-full bg-black' src="https://www.youtube.com/embed/kLBtY_rneF0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
+        <div className="relative h-[85vh] items-center overflow-hidden bg-gradient-to-bl from-orange-400 to-red-400 p-20 text-white backdrop-blur-2xl">
+          <div className="absolute top-10 left-[7%] pb-40 sm:top-[25%]">
+            <h1 className="text-6xl">Max Church</h1>
+            <h1 className="max-w-xl pr-10 text-2xl">
+              Artificial Intelligence With Computer Science student at the
+              University Of Sheffield.
+            </h1>
+            <h1 className="max-w-xl pr-5 pt-5 text-2xl transition-all hover:scale-110 ">
+              Full stack web devellopment with Next.js
+            </h1>
           </div>
 
-          <div className='px-6 text-white space-y-5 flex flex-col'>
-            <h1 className='text-4xl text-center'>Skills</h1>
-            
-            <Skill src="/skillIcons/react.png" title="React" caption="Started learning in Early 2022"/>
-            <Skill src="/skillIcons/next.png" title="Next" caption="Started learning in Early 2022"/>
-            <Skill src="/skillIcons/tailwind.png" title="Tailwind CSS" caption="Started learning in Early 2022"/>
-            <Skill src="/skillIcons/python.png" title="Python" caption="Used educationally and privately since 2017"/>
-            <Skill src="/skillIcons/java.png" title="Java" caption="Used in university, recieved a 1st class in Java module"/>
-            <Skill src="/skillIcons/ruby.png" title="Ruby" caption="Used in university for web devellopment module"/>
-            
+          <div className="absolute right-[5%] top-[20%] hidden aspect-video w-1/3 -translate-y-1/2 animate-blob animate-superGlow rounded-xl bg-white opacity-50 blur-xl lg:block"></div>
+          <div className="absolute right-[5%] top-[20%] hidden aspect-video w-1/3 -translate-y-1/2 animate-blob overflow-hidden rounded-lg blur-lg lg:block lg:blur-0">
+            <Image src={"/window.png"} width={1920} height={1140}></Image>
           </div>
 
+          <div>
+            <ChevronDownIcon className="absolute left-1/2 bottom-0 aspect-square h-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white p-2 text-red-300 transition-colors hover:text-purple-300" />
+          </div>
+        </div>
 
-          
-        </main>
-
-
+        <div className="h-[95vh] items-center overflow-hidden bg-red-100 p-20 text-white backdrop-blur-2xl"></div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,39 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+        superGlow: "superGlow 6s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "rotate(-1deg)",
+          },
+          "50%": {
+            transform: "rotate(1deg)",
+          },
+          "100%": {
+            transform: "rotate(-1deg)",
+          },
+        },
+        superGlow: {
+          "0%": {
+            transform: "scale(120%)",
+          },
+          "50%": {
+            transform: "scale(100%)",
+          },
+          "100%": {
+            transform: "scale(120%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
